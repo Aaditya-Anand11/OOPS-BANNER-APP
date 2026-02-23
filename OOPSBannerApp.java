739f063f469c06ecc9a04e19cb8fc540fccb978a
Aaditya-Anand11 <aa5256@srmist.edu.in>
2026-02-23 09:29:52 +0530

    UC2: Fix OOPS banner pattern with correct asterisk art

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,15 @@
+/**
+ * OOPS Banner App
+ * UC2: Render OOPS banner using string concatenation
+ * @author YourName
+ * @version 2.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("*****  ****  *****  *****");
-        System.out.println("*   *  *  *  *      *    ");
-        System.out.println("*   *  *  *  *****  *****");
-        System.out.println("*   *  *  *      *      *");
-        System.out.println("*****  ****  *****  *****");
+        System.out.println(" ***   ***  ****   **** ");
+        System.out.println("*   * *   * *   * *     ");
+        System.out.println("*   * *   * ****   ***  ");
+        System.out.println("*   * *   * *         * ");
+        System.out.println(" ***   ***  *     **** ");
     }
 }
