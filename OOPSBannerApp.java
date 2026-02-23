f139b7ee21aa4a0ff034bc77744b8dc7e60599c3
Aaditya-Anand11 <aa5256@srmist.edu.in>
2026-02-23 09:36:18 +0530

    UC8: Use Map for character patterns and render via function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,43 +1,57 @@
 /**
  * OOPS Banner App
- * UC7: Refactor banner using Inner Class
+ * UC8: Use Map for character patterns and render via function
  * @author YourName
- * @version 7.0
+ * @version 8.0
  */
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    static class CharacterPatternMap {
-        private char character;
-        private String[] pattern;
+    private static Map<Character, String[]> buildPatternMap() {
+        Map<Character, String[]> map = new LinkedHashMap<>();
 
-        public CharacterPatternMap(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
+        map.put('O', new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        });
 
-        public char getCharacter() {
-            return character;
-        }
+        map.put('P', new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        });
 
-        public String[] getPattern() {
-            return pattern;
+        map.put('S', new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        });
+
+        return map;
+    }
+
+    public static void renderBanner(String word) {
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        for (int row = 0; row < 5; row++) {
+            StringBuilder line = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                line.append(patternMap.get(ch)[row]).append("  ");
+            }
+            System.out.println(line);
         }
     }
 
     public static void main(String[] args) {
-        CharacterPatternMap[] letters = {
-            new CharacterPatternMap('O', new String[]{" *** ", "*   *", "*   *", "*   *", " *** "}),
-            new CharacterPatternMap('O', new String[]{" *** ", "*   *", "*   *", "*   *", " *** "}),
-            new CharacterPatternMap('P', new String[]{"**** ", "*   *", "**** ", "*    ", "*    "}),
-            new CharacterPatternMap('S', new String[]{" ****", "*    ", " *** ", "    *", "**** "})
-        };
-
-        for (int row = 0; row < 5; row++) {
-            StringBuilder line = new StringBuilder();
-            for (CharacterPatternMap letter : letters) {
-                line.append(letter.getPattern()[row]).append("  ");
-            }
-            System.out.println(line);
-        }
+        renderBanner("OOPS");
     }
 }
