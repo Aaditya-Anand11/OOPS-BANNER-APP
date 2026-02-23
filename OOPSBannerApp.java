629fa90dde04afe4ee6b42e2660eb34390c2b107
Aaditya Anand <aadityaanandskill11@gmail.com>
2026-02-23 15:35:59 +0530

    UC4: Fix - Render OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,21 @@
 /**
  * OOPS Banner App
- * UC3: Render OOPS banner using String.join()
- * @author YourName
- * @version 3.0
+ * UC4: Render OOPS banner using String Array and Loop
+ * @author Aaditya Anand
+ * @version 4.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println(String.join("  ", " *** ", " *** ", "**** ", " ****"));
-        System.out.println(String.join("  ", "*   *", "*   *", "*   *", "*    "));
-        System.out.println(String.join("  ", "*   *", "*   *", "**** ", " *** "));
-        System.out.println(String.join("  ", "*   *", "*   *", "*    ", "    *"));
-        System.out.println(String.join("  ", " *** ", " *** ", "*    ", "**** "));
+        String[] lines = new String[5];
+
+        lines[0] = String.join("  ", " *** ", " *** ", "**** ", " ****");
+        lines[1] = String.join("  ", "*   *", "*   *", "*   *", "*    ");
+        lines[2] = String.join("  ", "*   *", "*   *", "**** ", " *** ");
+        lines[3] = String.join("  ", "*   *", "*   *", "*    ", "    *");
+        lines[4] = String.join("  ", " *** ", " *** ", "*    ", "**** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
